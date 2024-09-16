ac1419215dac6e4627bc77d9fa0102b87eb372d6
joakimfr <fritzjoakim@hotmail.com>
2024-09-16 20:11:00 +0200

    starting on footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -4,8 +4,18 @@ import Link from "next/link";
 export const Footer = () => {
   return (
     <div className="footer">
-      <p>Footer</p>
-      <p>hallå</p>
+      <ul>
+        <h2>Kontakta oss</h2>
+        <li>0733454342</li>
+        <li>mail@hotmail.com</li>
+        <li>adress 35b</li>
+      </ul>
+      <ul>
+        <li>hem</li>
+        <li>Tjänster</li>
+        <li>Kontakt</li>
+        <li>Om oss</li>
+      </ul>
     </div>
-  )
-}
+  );
+};
